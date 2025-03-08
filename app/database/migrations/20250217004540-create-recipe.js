b96b0bb08fa2d83434d799aa102e1c741566e877
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2025-03-08 16:10:05 -0500

    Agregando softdelete a la tabala recetas

--- app/database/migrations/20250217004540-create-recipe.js
+++ app/database/migrations/20250217004540-create-recipe.js
@@ -48,7 +48,11 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
-      }
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
     });
   },
   async down(queryInterface, Sequelize) {
--- app/models/Recipe.js
+++ app/models/Recipe.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Recipe extends Model {
     /**
@@ -12,50 +10,62 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Recipe.belongsTo(models.Business, {
-        foreignKey: 'business_id',
-        as: 'business',
+        foreignKey: "business_id",
+        as: "business",
       });
       // Relación con Person
       Recipe.belongsTo(models.Person, {
-        foreignKey: 'person_id',
-        as: 'person',
+        foreignKey: "person_id",
+        as: "person",
       });
     }
   }
-  Recipe.init({
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true, // Indica que 'id' es la clave primaria
-      autoIncrement: true, // Esto hace que el campo 'id' sea auto-incrementable
+  Recipe.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, // Indica que 'id' es la clave primaria
+        autoIncrement: true, // Esto hace que el campo 'id' sea auto-incrementable
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+          len: {
+            args: [0, 500],
+            msg: "La descripción no puede exceder los 500 caracteres",
+          },
+        },
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      business_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      person_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      validate: {
-        len: { args: [0, 500], msg: 'La descripción no puede exceder los 500 caracteres' }
-      }
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    business_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    person_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  }, {
-    sequelize,
-    modelName: 'Recipe',
-    tableName: 'recipes',
-    timestamps: true
-  });
+    {
+      sequelize,
+      modelName: "Recipe",
+      tableName: "recipes",
+      timestamps: true,
+      paranoid: true, // Habilita el soft delete
+      scopes: {
+        withDeleted: {
+          paranoid: false, // Scope personalizado para incluir eliminados
+        },
+      },
+    }
+  );
   return Recipe;
 };
